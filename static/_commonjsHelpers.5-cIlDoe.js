a5e67a6e8cc5679f5bf1189356c56ddccb497b08
miguelroot <miguelkillki@gmail.com>
2024-10-03 05:48:59 -0500

    Updates

--- static/_commonjsHelpers.5-cIlDoe.js
+++ static/_commonjsHelpers.5-cIlDoe.js
@@ -1,5 +1,18 @@
-if (typeof globalThis === "undefined") {
-  var globalThis = (function () {
-    return this || (0, eval)("this");
-  })();
+var o =
+  typeof globalThis !== "undefined"
+    ? globalThis
+    : typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : typeof self !== "undefined"
+    ? self
+    : {};
+
+function l(e) {
+  return e && e.__esModule && Object.prototype.hasOwnProperty.call(e, "default")
+    ? e.default
+    : e;
 }
+
+module.exports = { c: o, g: l };
